a98f46ad933eacd53348b7583f71701d86ebcb7f
Nustakit <nuttakitkongkaew@gmail.com>
2025-09-07 20:22:55 +0700

    Add files via upload

---
+++ project final/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+FILE *file = fopen("tools.CVS", "r");
+if (file == NULL) {
+    printf("ไม่สามารถเปิดไฟล์ได้\n");
+    return 1;
+}
+
+int ch;
+while ((ch = fgetc(file)) != EOF) {
+    putchar(ch);  // แสดงตัวอักษรที่อ่านได้
+}
+
+fclose(file);
+}
